f76943aa8526b505cc41b5862982ccdb78987914
Shubham More <shubhammore1796@gmail.com>
2020-08-01 23:04:42 +0530

    eduatlas

---
+++ platforms/android/app/src/main/assets/www/common.3d91e00ac40c8d1c031f.js
@@ -0,0 +1,282 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["common"],{
+
+/***/ "./src/app/pipe/safe-html.pipe.ts":
+/*!****************************************!*\
+  !*** ./src/app/pipe/safe-html.pipe.ts ***!
+  \****************************************/
+/*! exports provided: SafeHtmlPipe */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "SafeHtmlPipe", function() { return SafeHtmlPipe; });
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm5/core.js");
+/* harmony import */ var _angular_platform_browser__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @angular/platform-browser */ "./node_modules/@angular/platform-browser/fesm5/platform-browser.js");
+
+
+/**
+ * Generated class for the SafeHtmlPipe pipe.
+ *
+ * See https://angular.io/api/core/Pipe for more info on Angular Pipes.
+ */
+var SafeHtmlPipe = /** @class */ /*@__PURE__*/ (function () {
+    function SafeHtmlPipe(sanitizer) {
+        this.sanitizer = sanitizer;
+    }
+    SafeHtmlPipe.prototype.transform = function (html) {
+        return this.sanitizer.bypassSecurityTrustHtml(html);
+    };
+    return SafeHtmlPipe;
+}());
+
+
+
+/***/ }),
+
+/***/ "./src/app/services/communication/announcement.service.ts":
+/*!****************************************************************!*\
+  !*** ./src/app/services/communication/announcement.service.ts ***!
+  \****************************************************************/
+/*! exports provided: AnnouncementService */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "AnnouncementService", function() { return AnnouncementService; });
+/* harmony import */ var _environments_environment__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./../../../environments/environment */ "./src/environments/environment.ts");
+/* harmony import */ var _angular_common_http__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @angular/common/http */ "./node_modules/@angular/common/fesm5/http.js");
+/* harmony import */ var rxjs_operators__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! rxjs/operators */ "./node_modules/rxjs/_esm5/operators/index.js");
+/* harmony import */ var rxjs__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! rxjs */ "./node_modules/rxjs/_esm5/index.js");
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm5/core.js");
+
+
+
+
+
+
+var AnnouncementService = /** @class */ /*@__PURE__*/ (function () {
+    function AnnouncementService(http) {
+        this.http = http;
+    }
+    AnnouncementService.prototype.editAnnouncement = function (announcement) {
+        var _this = this;
+        return this.http
+            .post(_environments_environment__WEBPACK_IMPORTED_MODULE_0__["environment"].server + "/institute/announcement/editAnnouncement", announcement)
+            .pipe(Object(rxjs_operators__WEBPACK_IMPORTED_MODULE_2__["tap"])(function (response) { }), Object(rxjs_operators__WEBPACK_IMPORTED_MODULE_2__["catchError"])(function (err) { return _this.handleError(err); }));
+    };
+    AnnouncementService.prototype.postAnnouncement = function (announcement) {
+        var _this = this;
+        return this.http
+            .post(_environments_environment__WEBPACK_IMPORTED_MODULE_0__["environment"].server + "/institute/announcement/makeAnnouncement", announcement)
+            .pipe(Object(rxjs_operators__WEBPACK_IMPORTED_MODULE_2__["tap"])(function (response) { }), Object(rxjs_operators__WEBPACK_IMPORTED_MODULE_2__["catchError"])(function (err) { return _this.handleError(err); }));
+    };
+    AnnouncementService.prototype.getAnnouncements = function (id) {
+        var _this = this;
+        return this.http
+            .post(_environments_environment__WEBPACK_IMPORTED_MODULE_0__["environment"].server + "/institute/announcement/getAnnouncement", { instituteId: id })
+            .pipe(Object(rxjs_operators__WEBPACK_IMPORTED_MODULE_2__["tap"])(function (res) { }), Object(rxjs_operators__WEBPACK_IMPORTED_MODULE_2__["catchError"])(function (err) { return _this.handleError(err); }));
+    };
+    AnnouncementService.prototype.getStudentAnnouncements = function (id, batch) {
+        var _this = this;
+        return this.http
+            .post(_environments_environment__WEBPACK_IMPORTED_MODULE_0__["environment"].server + "/institute/announcement/getStudentAnnouncements", {
+            instituteId: id,
+            batch: batch,
+        })
+            .pipe(Object(rxjs_operators__WEBPACK_IMPORTED_MODULE_2__["tap"])(function (res) { }), Object(rxjs_operators__WEBPACK_IMPORTED_MODULE_2__["catchError"])(function (err) { return _this.handleError(err); }));
+    };
+    AnnouncementService.prototype.getSingleAnnouncement = function (id) {
+        var _this = this;
+        return this.http
+            .post(_environments_environment__WEBPACK_IMPORTED_MODULE_0__["environment"].server + "/institute/announcement/getSingleAnnouncement", {
+            id: id,
+        })
+            .pipe(Object(rxjs_operators__WEBPACK_IMPORTED_MODULE_2__["tap"])(function (res) { }), Object(rxjs_operators__WEBPACK_IMPORTED_MODULE_2__["catchError"])(function (err) { return _this.handleError(err); }));
+    };
+    AnnouncementService.prototype.deleteAnnouncement = function (id, instituteId) {
+        var _this = this;
+        return this.http
+            .post(_environments_environment__WEBPACK_IMPORTED_MODULE_0__["environment"].server + "/institute/announcement/deleteAnnouncement", {
+            _id: id,
+            instituteId: instituteId,
+        })
+            .pipe(Object(rxjs_operators__WEBPACK_IMPORTED_MODULE_2__["tap"])(function (res) { }), Object(rxjs_operators__WEBPACK_IMPORTED_MODULE_2__["catchError"])(function (err) { return _this.handleError(err); }));
+    };
+    AnnouncementService.prototype.handleError = function (error) {
+        return Object(rxjs__WEBPACK_IMPORTED_MODULE_3__["throwError"])(error);
+    };
+    AnnouncementService.ngInjectableDef = _angular_core__WEBPACK_IMPORTED_MODULE_4__["ɵɵdefineInjectable"]({ factory: function AnnouncementService_Factory() { return new AnnouncementService(_angular_core__WEBPACK_IMPORTED_MODULE_4__["ɵɵinject"](_angular_common_http__WEBPACK_IMPORTED_MODULE_1__["HttpClient"])); }, token: AnnouncementService, providedIn: "root" });
+    return AnnouncementService;
+}());
+
+
+
+/***/ }),
+
+/***/ "./src/app/services/meeting.service.ts":
+/*!*********************************************!*\
+  !*** ./src/app/services/meeting.service.ts ***!
+  \*********************************************/
+/*! exports provided: MeetingService */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "MeetingService", function() { return MeetingService; });
+/* harmony import */ var _angular_common_http__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @angular/common/http */ "./node_modules/@angular/common/fesm5/http.js");
+/* harmony import */ var rxjs_operators__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! rxjs/operators */ "./node_modules/rxjs/_esm5/operators/index.js");
+/* harmony import */ var rxjs__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! rxjs */ "./node_modules/rxjs/_esm5/index.js");
+/* harmony import */ var _environments_environment__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ../../environments/environment */ "./src/environments/environment.ts");
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm5/core.js");
+
+
+
+
+
+
+var MeetingService = /** @class */ /*@__PURE__*/ (function () {
+    function MeetingService(http) {
+        this.http = http;
+    }
+    MeetingService.prototype.getAllMeetingLinks = function (instituteId) {
+        var url = _environments_environment__WEBPACK_IMPORTED_MODULE_3__["environment"].server + "/institute/meeting/getAllMeetingLinks";
+        return this.http.post(url, { instituteId: instituteId }).pipe(Object(rxjs_operators__WEBPACK_IMPORTED_MODULE_1__["tap"])(function (res) { }), Object(rxjs_operators__WEBPACK_IMPORTED_MODULE_1__["catchError"])(this.handleError));
+    };
+    MeetingService.prototype.deleteMeetingLink = function (data) {
+        var url = _environments_environment__WEBPACK_IMPORTED_MODULE_3__["environment"].server + "/institute/meeting/deleteMeetingLink";
+        return this.http.post(url, data).pipe(Object(rxjs_operators__WEBPACK_IMPORTED_MODULE_1__["tap"])(function (res) { }), Object(rxjs_operators__WEBPACK_IMPORTED_MODULE_1__["catchError"])(this.handleError));
+    };
+    MeetingService.prototype.updateMeetingLink = function (data) {
+        var url = _environments_environment__WEBPACK_IMPORTED_MODULE_3__["environment"].server + "/institute/meeting/updateMeetingLink";
+        return this.http.post(url, data).pipe(Object(rxjs_operators__WEBPACK_IMPORTED_MODULE_1__["tap"])(function (res) { }), Object(rxjs_operators__WEBPACK_IMPORTED_MODULE_1__["catchError"])(this.handleError));
+    };
+    MeetingService.prototype.createMeetingLink = function (data) {
+        var url = _environments_environment__WEBPACK_IMPORTED_MODULE_3__["environment"].server + "/institute/meeting/createMeetingLink";
+        return this.http.post(url, data).pipe(Object(rxjs_operators__WEBPACK_IMPORTED_MODULE_1__["tap"])(function (res) { }), Object(rxjs_operators__WEBPACK_IMPORTED_MODULE_1__["catchError"])(this.handleError));
+    };
+    MeetingService.prototype.getOneMeetingLink = function (data) {
+        var url = _environments_environment__WEBPACK_IMPORTED_MODULE_3__["environment"].server + "/institute/meeting/getOneMeetingLink";
+        return this.http.post(url, data).pipe(Object(rxjs_operators__WEBPACK_IMPORTED_MODULE_1__["tap"])(function (res) { }), Object(rxjs_operators__WEBPACK_IMPORTED_MODULE_1__["catchError"])(this.handleError));
+    };
+    MeetingService.prototype.getMeetingLinkByBatch = function (data) {
+        var url = _environments_environment__WEBPACK_IMPORTED_MODULE_3__["environment"].server + "/institute/meeting/getMeetingLinks";
+        return this.http.post(url, data).pipe(Object(rxjs_operators__WEBPACK_IMPORTED_MODULE_1__["tap"])(function (res) { }), Object(rxjs_operators__WEBPACK_IMPORTED_MODULE_1__["catchError"])(this.handleError));
+    };
+    MeetingService.prototype.addRecording = function (data) {
+        var url = _environments_environment__WEBPACK_IMPORTED_MODULE_3__["environment"].server + "/institute/meeting/addRecording";
+        return this.http.post(url, data).pipe(Object(rxjs_operators__WEBPACK_IMPORTED_MODULE_1__["tap"])(function (res) { }), Object(rxjs_operators__WEBPACK_IMPORTED_MODULE_1__["catchError"])(this.handleError));
+    };
+    MeetingService.prototype.deleteRecording = function (_id, recordingId, instituteId) {
+        var url = _environments_environment__WEBPACK_IMPORTED_MODULE_3__["environment"].server + "/institute/meeting/deleteRecording";
+        return this.http.post(url, { _id: _id, recordingId: recordingId, instituteId: instituteId }).pipe(Object(rxjs_operators__WEBPACK_IMPORTED_MODULE_1__["tap"])(function (res) { }), Object(rxjs_operators__WEBPACK_IMPORTED_MODULE_1__["catchError"])(this.handleError));
+    };
+    MeetingService.prototype.handleError = function (error) {
+        return Object(rxjs__WEBPACK_IMPORTED_MODULE_2__["throwError"])(error);
+    };
+    MeetingService.ngInjectableDef = _angular_core__WEBPACK_IMPORTED_MODULE_4__["ɵɵdefineInjectable"]({ factory: function MeetingService_Factory() { return new MeetingService(_angular_core__WEBPACK_IMPORTED_MODULE_4__["ɵɵinject"](_angular_common_http__WEBPACK_IMPORTED_MODULE_0__["HttpClient"])); }, token: MeetingService, providedIn: "root" });
+    return MeetingService;
+}());
+
+
+
+/***/ }),
+
+/***/ "./src/app/services/schedule/schedule.service.ts":
+/*!*******************************************************!*\
+  !*** ./src/app/services/schedule/schedule.service.ts ***!
+  \*******************************************************/
+/*! exports provided: ScheduleService */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "ScheduleService", function() { return ScheduleService; });
+/* harmony import */ var _environments_environment__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./../../../environments/environment */ "./src/environments/environment.ts");
+/* harmony import */ var _angular_common_http__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @angular/common/http */ "./node_modules/@angular/common/fesm5/http.js");
+/* harmony import */ var rxjs__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! rxjs */ "./node_modules/rxjs/_esm5/index.js");
+/* harmony import */ var rxjs_operators__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! rxjs/operators */ "./node_modules/rxjs/_esm5/operators/index.js");
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm5/core.js");
+
+
+
+
+
+
+var ScheduleService = /** @class */ /*@__PURE__*/ (function () {
+    function ScheduleService(http) {
+        this.http = http;
+    }
+    ScheduleService.prototype.addSchedule = function (schedule) {
+        return this.http.post(_environments_environment__WEBPACK_IMPORTED_MODULE_0__["environment"].server + "/institute/schedule/addSchedule", schedule).pipe(Object(rxjs_operators__WEBPACK_IMPORTED_MODULE_3__["tap"])(function (res) { }), Object(rxjs_operators__WEBPACK_IMPORTED_MODULE_3__["catchError"])(this.handleError));
+    };
+    ScheduleService.prototype.updateSchedule = function (schedule, scheduleId) {
+        schedule._id = scheduleId;
+        return this.http.post(_environments_environment__WEBPACK_IMPORTED_MODULE_0__["environment"].server + "/institute/schedule/updateSchedule", schedule).pipe(Object(rxjs_operators__WEBPACK_IMPORTED_MODULE_3__["tap"])(function (res) { }), Object(rxjs_operators__WEBPACK_IMPORTED_MODULE_3__["catchError"])(this.handleError));
+    };
+    ScheduleService.prototype.getScheduleByInstitute = function (data) {
+        return this.http
+            .post(_environments_environment__WEBPACK_IMPORTED_MODULE_0__["environment"].server + "/institute/schedule/getScheduleByInstitute", data)
+            .pipe(Object(rxjs_operators__WEBPACK_IMPORTED_MODULE_3__["catchError"])(this.handleError));
+    };
+    ScheduleService.prototype.getScheduleByBatch = function (instituteId, courseId, batchId) {
+        return this.http
+            .post(_environments_environment__WEBPACK_IMPORTED_MODULE_0__["environment"].server + "/institute/schedule/getScheduleByBatch", {
+            instituteId: instituteId,
+            courseId: courseId,
+            batchId: batchId,
+        })
+            .pipe(Object(rxjs_operators__WEBPACK_IMPORTED_MODULE_3__["catchError"])(this.handleError));
+    };
+    ScheduleService.prototype.getSchedule = function (scheduleId) {
+        return this.http
+            .post(_environments_environment__WEBPACK_IMPORTED_MODULE_0__["environment"].server + "/institute/schedule/getSchedule", {
+            scheduleId: scheduleId,
+        })
+            .pipe(Object(rxjs_operators__WEBPACK_IMPORTED_MODULE_3__["catchError"])(this.handleError));
+    };
+    ScheduleService.prototype.getScheduleDetails = function (scheduleId) {
+        return this.http
+            .post(_environments_environment__WEBPACK_IMPORTED_MODULE_0__["environment"].server + "/institute/schedule/getScheduleDetails", {
+            scheduleId: scheduleId,
+        })
+            .pipe(Object(rxjs_operators__WEBPACK_IMPORTED_MODULE_3__["catchError"])(this.handleError));
+    };
+    ScheduleService.prototype.deleteSchedule = function (scheduleId) {
+        return this.http
+            .post(_environments_environment__WEBPACK_IMPORTED_MODULE_0__["environment"].server + "/institute/schedule/deleteSchedule", {
+            scheduleId: scheduleId,
+        })
+            .pipe(Object(rxjs_operators__WEBPACK_IMPORTED_MODULE_3__["catchError"])(this.handleError));
+    };
+    ScheduleService.prototype.handleError = function (error) {
+        return Object(rxjs__WEBPACK_IMPORTED_MODULE_2__["throwError"])(error);
+    };
+    ScheduleService.ngInjectableDef = _angular_core__WEBPACK_IMPORTED_MODULE_4__["ɵɵdefineInjectable"]({ factory: function ScheduleService_Factory() { return new ScheduleService(_angular_core__WEBPACK_IMPORTED_MODULE_4__["ɵɵinject"](_angular_common_http__WEBPACK_IMPORTED_MODULE_1__["HttpClient"])); }, token: ScheduleService, providedIn: "root" });
+    return ScheduleService;
+}());
+
+
+
+/***/ }),
+
+/***/ "./src/app/services/shared.module.ts":
+/*!*******************************************!*\
+  !*** ./src/app/services/shared.module.ts ***!
+  \*******************************************/
+/*! exports provided: SharedModule */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "SharedModule", function() { return SharedModule; });
+var SharedModule = /** @class */ /*@__PURE__*/ (function () {
+    function SharedModule() {
+    }
+    return SharedModule;
+}());
+
+
+
+/***/ })
+
+}]);
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   razorpayKeyId: 'rzp_test_EVmv0tdRskXUYb',
-  server: '',
-  SOCKET_ENDPOINT: '/eduatlas-chat',
+  server: 'https://lms.eduatlas.in',
+  SOCKET_ENDPOINT: 'https://lms.eduatlas.in/eduatlas-chat',
 };
