3a1c437747507c694e3962dc4bfeba9d54e1a0cd
sarvesh <sarvesh10n@gmail.com>
2020-08-30 19:06:27 +0530

    bug fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  razorpayKeyId: 'rzp_test_EVmv0tdRskXUYb',
+  razorpayKeyId: 'rzp_live_S6sf8sC0WKjioL',
   server: 'https://lms.eduatlas.in',
   SOCKET_ENDPOINT: 'https://lms.eduatlas.in/eduatlas-chat',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  razorpayKeyId: 'rzp_test_EVmv0tdRskXUYb',
+  razorpayKeyId: 'rzp_live_S6sf8sC0WKjioL',
   server: 'http://localhost:5000',
   SOCKET_ENDPOINT: 'http://localhost:5000/eduatlas-chat',
   // SOCKET_ENDPOINT: '/eduatlas-chat',
